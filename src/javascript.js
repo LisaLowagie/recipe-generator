b18843063fbf75f5ede37027b3f8eca72c61b562
LisaLowagie <lowagielisa@gmail.com>
2024-10-27 12:08:37 +0100

    Added basic js form + typewriter plugin

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: ["Searching for recipe"],
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement = addEventListener("submit", generateRecipe);
